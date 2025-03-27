df934153ecf7e916d20729be9d1570cf3ace16c4
loeshat <leoshii111@gmail.com>
2025-03-27 11:37:04 +1100

    omg so dumb

--- dashboardWebsite/frontend/src/api.js
+++ dashboardWebsite/frontend/src/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL =
   // import.meta.env.MODE === "development"
   //   ? "http://localhost:8080"
   //   : import.meta.env.VITE_API_URL;
-  import.meta.env.VITE_API_IRL;
+  import.meta.env.VITE_API_URL;
 
 // 🔹 Reusable function to trigger any workflow
 export const triggerWorkflow = async (workflowKey, authUser) => {
